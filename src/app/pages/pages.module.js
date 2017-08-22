e246a864f42a4a6259fbc5b2f721d7049552170a
ganeshkumar.k@tevatel.com <ganeshkumar.k@tevatel.com>
2017-08-22 22:08:04 +0530

    branch-User

--- src/app/pages/pages.module.js
+++ src/app/pages/pages.module.js
@@ -12,7 +12,7 @@
     'BlurAdmin.pages.ui',
     'BlurAdmin.pages.components',
     'BlurAdmin.pages.form',
-    'BlurAdmin.pages.tables',
+    'BlurAdmin.pages.user',
     'BlurAdmin.pages.charts',
     'BlurAdmin.pages.maps',
     'BlurAdmin.pages.profile',
---
+++ src/app/pages/user/tables.module.js
@@ -0,0 +1,27 @@
+/**
+ * @author v.lugovsky
+ * created on 16.12.2015
+ */
+(function () {
+  'use strict';
+
+  angular.module('BlurAdmin.pages.user', [])
+    .config(routeConfig);
+
+  /** @ngInject */
+  function routeConfig($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('user', {
+          url: '/user',
+          templateUrl: 'app/pages/user/basic/user.html',
+          title: 'Users',
+          controller: 'UserController',
+          sidebarMeta: {
+            icon: 'fa fa-user',
+            order: 300
+          },
+        });
+    $urlRouterProvider.when('/user','/user');
+  }
+
+})();
