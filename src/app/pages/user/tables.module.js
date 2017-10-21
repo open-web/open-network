8a5052f8e6f8dc1a7297e204753f50ba7107c6cb
ishwarya <ishwarya@tevatel.com>
2017-10-21 17:46:55 +0530

    userUI_integrated

--- src/app/pages/user/tables.module.js
+++ src/app/pages/user/tables.module.js
@@ -2,26 +2,26 @@
  * @author v.lugovsky
  * created on 16.12.2015
  */
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  angular.module('BlurAdmin.pages.user', [])
-    .config(routeConfig);
+    angular.module('BlurAdmin.pages.user', [])
+        .config(routeConfig);
 
-  /** @ngInject */
-  function routeConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-        .state('user', {
-          url: '/user',
-          templateUrl: 'app/pages/user/basic/user.html',
-          title: 'Users',
-          controller: 'UserController',
-          sidebarMeta: {
-            icon: 'fa fa-user',
-            order: 300
-          },
-        });
-    $urlRouterProvider.when('/user','/user');
-  }
+    /** @ngInject */
+    function routeConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('user', {
+                url: '/user',
+                templateUrl: 'app/pages/user/basic/user.html',
+                title: 'Users',
+                controller: 'UserController as userCtrl',
+                sidebarMeta: {
+                    icon: 'fa fa-user',
+                    order: 300
+                },
+            });
+        $urlRouterProvider.when('/user', '/user');
+    }
 
 })();
